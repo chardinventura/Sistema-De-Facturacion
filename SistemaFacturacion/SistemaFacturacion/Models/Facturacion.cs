8eff94c1b5c5474a4d71c756cefc3d93e49f0ad4
Chardin De La Cruz Ventura <chardindelacruzventura829@hotmail.com>
2020-04-07 16:58:19 -0400

    Se agregaron los modelos que alimentaron los modulos

---
+++ SistemaFacturacion/SistemaFacturacion/Models/Facturacion.cs
@@ -0,0 +1,26 @@
+namespace SistemaFacturacion.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Facturacion")]
+    public partial class Facturacion
+    {
+        public int Id { get; set; }
+
+        public int? cliente_id { get; set; }
+
+        public decimal? Descuento { get; set; }
+
+        public decimal? Itbis { get; set; }
+
+        public decimal? Monto { get; set; }
+
+        public DateTime? Fecha { get; set; }
+
+        public virtual Cliente Cliente { get; set; }
+    }
+}
