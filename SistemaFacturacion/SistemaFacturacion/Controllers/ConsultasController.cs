2fde286c4e61c0f00fc23bfcd30dd551c2ce35f4
Chardin De La Cruz Ventura <chardindelacruzventura829@hotmail.com>
2020-04-07 20:46:58 -0400

    Se agrego el controlador de consultas

---
+++ SistemaFacturacion/SistemaFacturacion/Controllers/ConsultasController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SistemaFacturacion.Models;
+using System.Web.Mvc;
+using System.Data.Entity;
+using System.Linq;
+
+namespace SistemaFacturacion.Controllers
+{
+    public class ConsultasController : Controller
+    {
+        private ModelSF db = new ModelSF();
+
+        // GET: Consultas
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Productos(string query)
+        {
+            if(query != "" && query != null)
+                return View(db.Productos.Where(p => p.Nombre == query));
+            else
+                return View(db.Productos.ToList());
+        }
+        public ActionResult Clientes()
+        {
+            return View();
+        }
+        public ActionResult Proveedores()
+        {
+            return View();
+        }
+        public ActionResult Entradas()
+        {
+            return View();
+        }
+        public ActionResult Facturaciones()
+        {
+            return View();
+        }
+    }
+}
